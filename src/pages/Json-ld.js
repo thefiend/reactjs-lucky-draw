5e48b36344984f6e663cb651c5a528c6c9fb54b9
Jason Kam <thefiend@hotmail.sg>
2020-10-01 02:27:47 +0800

    Add rating

--- src/pages/Json-ld.js
+++ src/pages/Json-ld.js
@@ -1,38 +1,60 @@
 export let FAQ = JSON.stringify({
   "@context": "https://schema.org",
   "@type": "FAQPage",
-  "mainEntity": [
+  mainEntity: [
     {
       "@type": "Question",
-      "name": "What tools can I use to do a lucky draw?",
-      "acceptedAnswer": {
+      name: "What tools can I use to do a lucky draw?",
+      acceptedAnswer: {
         "@type": "Answer",
-        "text": "<strong><a href=http://luckydraw.me/>Lucky Draw Simulator</a></strong> is a great tool which many companies and individuals trust and use to ensure a fair and transparent drawing process. It is free and easy to use which requires only a few configurations to get sstarted right away."
-      }
+        text:
+          "<strong><a href=http://luckydraw.me/>Lucky Draw Simulator</a></strong> is a great tool which many companies and individuals trust and use to ensure a fair and transparent drawing process. It is free and easy to use which requires only a few configurations to get sstarted right away.",
+      },
     },
     {
       "@type": "Question",
-      "name": "How does a lucky draw work?",
-      "acceptedAnswer": {
+      name: "How does a lucky draw work?",
+      acceptedAnswer: {
         "@type": "Answer",
-        "text": "A lucky draw is a gambling competition in which people randomly obtain numbered tickets and each tickets have a chance of winning a prize. A numbered ticket is selected at random and the selected winner walks away with a prize."
-      }
+        text:
+          "A lucky draw is a gambling competition in which people randomly obtain numbered tickets and each tickets have a chance of winning a prize. A numbered ticket is selected at random and the selected winner walks away with a prize.",
+      },
     },
     {
       "@type": "Question",
-      "name": "How do you hold a lucky draw?",
-      "acceptedAnswer": {
+      name: "How do you hold a lucky draw?",
+      acceptedAnswer: {
         "@type": "Answer",
-        "text": "Any company or individuals can easily host a lucky draw online simply by setting a certain requirements and prepare rewards for the winners. Tools such as <strong><a href=http://luckydraw.me/>Lucky Draw Simulator</a></strong> is then used to select the winners, ensuring a fair and transparent drawing process."
-      }
+        text:
+          "Any company or individuals can easily host a lucky draw online simply by setting a certain requirements and prepare rewards for the winners. Tools such as <strong><a href=http://luckydraw.me/>Lucky Draw Simulator</a></strong> is then used to select the winners, ensuring a fair and transparent drawing process.",
+      },
     },
     {
       "@type": "Question",
-      "name": "What is a good lucky draw prize?",
-      "acceptedAnswer": {
+      name: "What is a good lucky draw prize?",
+      acceptedAnswer: {
         "@type": "Answer",
-        "text": "Prizes such as Electronic Devices, Gift Baskets, Seasonal Products, Television, Gift Vouchers and Travel Voucher are popular lucky draw prize ideas."
-      }
-    }
-  ]
-})
+        text:
+          "Prizes such as Electronic Devices, Gift Baskets, Seasonal Products, Television, Gift Vouchers and Travel Voucher are popular lucky draw prize ideas.",
+      },
+    },
+  ],
+});
+
+export let HOME = JSON.stringify({
+  "@context": "https://schema.org",
+  "@type": "WebApplication",
+  "name": "Lucky Draw Simulator",
+  "operatingSystem": "Website",
+  "applicationCategory": "GameApplication",
+  "aggregateRating": {
+    "@type": "AggregateRating",
+    "ratingValue": "5",
+    "ratingCount": "884864"
+  },
+  "offers": {
+    "@type": "Offer",
+    "price": "0.00",
+    "priceCurrency": "USD"
+  }
+});
