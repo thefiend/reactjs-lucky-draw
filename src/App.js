5ffd1cca26c4d76fe04a85704b7c3958a7729cee
thefiend <kam_ming_feng@tech.gov.sg>
2023-10-05 01:26:40 +0800

    Regenerate package-lock

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "tabler-react/dist/Tabler.css";
 
 import * as React from "react";
-import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 
 import Faq from "./pages/faq/Faq";
 import Home from "./pages/home/Home";
@@ -11,11 +11,11 @@ function App() {
   return (
     <React.StrictMode>
       <Router>
-        <Routes>
+        <Switch>
           <Route path="/list" element={<List />} />
           <Route path="/faq" element={<Faq />} />
           <Route exact path="/" element={<Home />} />
-        </Routes>
+        </Switch>
       </Router>
     </React.StrictMode>
   );
