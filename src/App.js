25878512adf87e8b351a5ebd05bcf049ced2c678
Jason Kam <kam_ming_feng@tech.gov.sg>
2024-08-14 16:42:11 +0800

    Remove FAQ page from routes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "tabler-react/dist/Tabler.css";
 import * as React from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 
-import Faq from "./pages/faq/Faq";
 import Home from "./pages/home/Home";
 import List from "./pages/list/List";
 
@@ -13,7 +12,6 @@ function App() {
       <Router>
         <Switch>
           <Route path="/list"><List /></Route>
-          <Route path="/faq"><Faq /></Route>
           <Route exact path="/"><Home /></Route>
         </Switch>
       </Router>
