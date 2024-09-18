6f43e711894fe128110e95080775a483a1b11d18
Ming Feng KAM <kam_ming_feng@tech.gov.sg>
2024-09-18 14:31:05 +0800

    Revert "Remove FAQ page from routes"
    This reverts commit 25878512adf87e8b351a5ebd05bcf049ced2c678.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "tabler-react/dist/Tabler.css";
 import * as React from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 
+import Faq from "./pages/faq/Faq";
 import Home from "./pages/home/Home";
 import List from "./pages/list/List";
 
@@ -12,6 +13,7 @@ function App() {
       <Router>
         <Switch>
           <Route path="/list"><List /></Route>
+          <Route path="/faq"><Faq /></Route>
           <Route exact path="/"><Home /></Route>
         </Switch>
       </Router>
